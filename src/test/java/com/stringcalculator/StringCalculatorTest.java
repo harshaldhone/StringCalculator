0b3975c7f082a08926fe131b0abe1887efc3b6f8
Harshal Dhone <harshaldhone99@gmail.com>
2021-07-13 22:39:48 +0530

    Added second test to check number return on Number input

--- src/test/java/com/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/stringcalculator/StringCalculatorTest.java
@@ -8,7 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorTest {
 
     @Test
+    @DisplayName("Empty string Test")
     public void shouldReturnZeroOnEmptyString(){
         assertEquals(0,StringCalculator.Add(""));
     }
+
+    @Test
+    @DisplayName("Number Return Test")
+    public void shouldReturnNumonNum(){
+        assertEquals(1,StringCalculator.Add("1"));
+    }
+
+
+
 }
