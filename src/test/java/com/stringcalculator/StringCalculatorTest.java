175d46d543aab62046ae4bc362d8f84732e4b5f0
Harshal Dhone <harshaldhone99@gmail.com>
2021-07-13 20:47:08 +0530

    Adding Test Class and run empty test to cheak setup

---
+++ src/test/java/com/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.stringcalculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    @Test
+    @DisplayName("This is Trial Test")
+    public void TrialTest(){
+
+    }
+}
