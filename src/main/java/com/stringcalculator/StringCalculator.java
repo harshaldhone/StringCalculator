c4e09c34bd77757050d68b456d9347b76d6737ef
Harshal Dhone <harshaldhone99@gmail.com>
2021-07-15 20:15:38 +0530

    Write test for more than 2 numbers and modify Add function

--- src/main/java/com/stringcalculator/StringCalculator.java
+++ src/main/java/com/stringcalculator/StringCalculator.java
@@ -1,17 +1,35 @@
 package com.stringcalculator;
 
+import java.util.List;
+
 public class StringCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello World...!");
+        try{
+            System.out.println(Add("0"));
+        }catch (Exception e){
+            System.out.println("Exception"+e);
+        }
     }
     public static int Add(String s){
 
+        int sum=0;
         if(s.isEmpty()){
             return 0;
         }
+        else if(s.contains(",")){
+            String[] tokens = s.split(",");
+            for(String strTmp:tokens){
+                sum=sum+toInt(strTmp);
+            }
+            return toInt(tokens[0])+toInt(tokens[1]);
+        }
         else {
-            return Integer.parseInt(s);
+            return toInt(s);
         }
 
+    }
+
+    private static int toInt(String numText) throws NumberFormatException{
+        return Integer.parseInt(numText);
     }
 }
--- src/test/java/com/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/stringcalculator/StringCalculatorTest.java
@@ -1,7 +1,11 @@
 package com.stringcalculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +23,17 @@ class StringCalculatorTest {
         assertEquals(1,StringCalculator.Add("1"));
     }
 
+    @Test
+    @DisplayName("Addition Return Test")
+    public void shouldReturnSumOfTwoNumbers(){
+        assertEquals(5,StringCalculator.Add("3,2"));
+    }
+
+    @Test
+    @DisplayName("Addition Return Test")
+    public void shouldReturnSumOfNumbers(){
+        assertEquals(15,StringCalculator.Add("3,2,10"));
+    }
 
 
 }
